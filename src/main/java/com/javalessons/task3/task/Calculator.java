a0cad1ad85b1e51c0a13726f46677ab24673a142
alla_kvitka <invariant17@gmail.com>
2021-09-01 17:17:31 +0300

    ++

--- src/main/java/com/javalessons/task3/task/Calculator.java
+++ src/main/java/com/javalessons/task3/task/Calculator.java
@@ -1,34 +1,17 @@
 package com.javalessons.task3.task;
-/*Для успешного выполнения задания Вам нужно будет рассчитать и вывести в несколько колонок
-следующие значения: "MONTH, STARING_BALANCE, PAYMENT, INTEREST, PRINCIPAL, ENDING_BALANCE,
-TOTAL_INTEREST". Где MONTH – номер платежного месяца (в случае если кредит
-оформлен на 10 лет то номера идут от 1 до 120),
-STARING_BALANCE – баланс на начало месяца
-(т.е сумма задолженности до того как мы внесли очередной платеж,
-в самом начале равна объему кредита), PAYMENT – размер платежа за период (месяц),
-INTEREST – какую сумму от платежа составляет процент который клиент платит банку в текущем месяце,
-PRINCIPAL – сумма которая уходит на погашение тела кредита, ENDING_BALANCE – сумма задолженности,
-которая остается на конец месяца, после того как был внесен очередной платеж
-(эту же сумму нужно вывести в качестве STARING_BALANCE в следующем цикле),
-TOTAL_INTEREST – сколько суммарно денег заплатил банку
-заемщик в качестве процентов за пользование кредитом на конец текущего периода. Для вывода в
-консоль отформатированного текста Вы можете использовать оператор printf. Пример:
 
-System.out.printf("%d, %15.2f, %12.2f, %8.2f, %8.2f, %15.2f, %15.2f%n", periodCount, monthStartBalance,
-        payment, monthlyInterest, monthlyPrincipal, monthEndBalance, interestAmount);
-%15.2f%n - тут %15 отвечает за отступ вправо при выводе переданного аргумента, .
-2 отвечает за количество десятичных знаков после запятой в случае использования типа с плавающей точкой,
- f - указывает что мы передаем значение типа с плавающей точкой,
- %n - знак переноса строки. Больше можно почитать тут: easycode*/
+import java.util.SortedMap;
 
 public class Calculator {
+
     //оголошення полів класа
     private static final int MONTHS = 12;
-    private  final int loanAmount;  // loanAmount (объем кредита)
-    private  final int years ;     //years (количество лет на которое берется ипотека)
-    private  final double interestRate; //interestRate (ежегодный процент под который банк выдает кредит)
-    private  final int extraPayment; //extraPayment (дополнительный ежемесячный платеж или же сумма, которую вносит плательщик сверх обязательной, например для ускоренного погашения).
-   //створення контрруктора для ініціалізації полів
+    private final int loanAmount;  // loanAmount (объем кредита)
+    private final int years;     //years (количество лет на которое берется ипотека)
+    private final double interestRate; //interestRate (ежегодный процент под который банк выдает кредит)
+    private final int extraPayment; //extraPayment (дополнительный ежемесячный платеж или же сумма, которую вносит плательщик сверх обязательной, например для ускоренного погашения).
+
+    //створення контрруктора для ініціалізації полів
     public Calculator(final int loanAmount, final int years, double interestRate, int extraPayment) {
         this.loanAmount = loanAmount;
         this.interestRate = interestRate;
@@ -36,8 +19,49 @@ public class Calculator {
         this.years = years;
     }
 
-    public void tableCalculate () {
+    //оголошення методу для розрахунку таблиці
+    public void tableCalculate() {
+        // виведення назв стовбців
         System.out.println("MONTH,  STARING_BALANCE,  PAYMENT,  INTEREST,  PRINCIPAL,  ENDING_BALANCE,  TOTAL_INTEREST");
+        //оголошення змінних
+        int months = years * MONTHS;
+        //period - номер платіжного місяця
+        int period = 0;
+        double startingBalance = loanAmount;
+        double payment = paymentCalculate(startingBalance, interestRate);
+        double interest = interestCalculate(startingBalance, interestRate);
+        double endingBalance = endingBalanceCalculate(startingBalance, interest);
+        double principal = 0;
+        double otalInterest = 0;
+
+        while (period < months) {
+            period++;
+
+            System.out.printf(period, startingBalance, payment, interest, principal, endingBalance, otalInterest);
+
+
+            startingBalance = endingBalance;
+            otalInterest = otalInterest + interest;
+            principal = principal + payment;
+
+
+        }
+    }
+
+    public double paymentCalculate(double startingBalance, double interestRate) {
+        return (startingBalance * interestRate) / 1 - Math.pow((1 - interestRate), -MONTHS * years);
+
+        public double interestCalculate ( double startingBalance, double interestRate){
+            return startingBalance * interestRate;
+        }
+
+        public double endingBalanceCalculate ( double startingBalance, double payment){
+            return loanAmount - interest;
+        }
+
+
     }
 
 }
+
+
--- src/main/java/com/javalessons/task3/task/MortgageCalc.java
+++ src/main/java/com/javalessons/task3/task/MortgageCalc.java
@@ -6,7 +6,7 @@ public class MortgageCalc {
     public static void main(String[] args) {
 
         Calculator customer1 = new Calculator(1000,4,0.5,0);
-
+        customer1.tableCalculate();
 
 
 
